b80ba224aed8da73ebf4de7f6f2250f73ab632d2
KatKlee <katklee2022@gmail.com>
2022-08-09 08:56:14 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const adresse = ['Groninger Straße', '13347', 'Berlin', 'Berlin']
+const freunde = ['Pia', 'Lisa', 'Michelle', 'Beni', 'Karsten', 'Andy', 'Andaras', 'Cyra']
+const person = ['Katrin', 'Kleemann', 'Käthe', 35, 'Tanzen & Musik', 'Wonderland', 'SuperCode']
+
+console.log('This is the array length of person: ' + person.length) // Ausgabe als String
+console.log(`This is the array length of person: ${person.length}`) // Ausgabe als String
+console.log('This is the array length of person: ', person.length) // Ausgabe als Number
+console.log('This is the array length of freunde: ' + freunde.length)
+console.log('This is the array length of adresse: ' + adresse.length)
